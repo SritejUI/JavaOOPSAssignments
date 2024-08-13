e71085e0a2bbfcf8aea5704331c9d26ee07eff79
SritejUI <sritejpaini14@gmail.com>
2024-08-13 15:44:45 +0530

    qs5.java

--- qs5.java
+++ qs5.java
@@ -7,27 +7,53 @@ class qs5 {
         int rows = sc.nextInt();
         System.out.println("No. of cols in matrix: ");
         int cols = sc.nextInt();
-        int[][] arr = new int[rows][cols];
+        int[][] matrix = new int[rows][cols];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 System.out.println("Enter element: ");
-                arr[i][j] = sc.nextInt();
+                matrix[i][j] = sc.nextInt();
             }
         }
 
         System.out.println("Matix is given by: ");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                System.out.print(arr[i][j] + " ");
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
 
-        System.out.println("Traversal: ");
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.print(arr[i][j] + " ");
+        int total = rows*cols;
+        int cnt = 0;
+        int startingRow = 0;
+        int startingCol = 0;
+        int endingRow = rows - 1;
+        int endingCol = cols - 1;
+        int[] ans = new int[total];
+
+        while (cnt < total) {
+            for (int i = startingCol; cnt < total && i <= endingCol; i++) {
+                ans[cnt++] = matrix[startingRow][i];
             }
+            startingRow++;
+            for (int i = startingRow; cnt < total && i <= endingRow; i++) {
+                ans[cnt++] = matrix[i][endingCol];
+            }
+            endingCol--;
+            for (int i = endingCol; cnt < total && i >= startingCol; i--) {
+                ans[cnt++] = matrix[endingRow][i];
+            }
+            endingRow--;
+            for (int i = endingRow; cnt < total && i >= startingRow; i--) {
+                ans[cnt++] = matrix[i][startingCol];
+            }
+            startingCol++;
         }
+
+        System.out.println("Spiral traversal: ");
+        for (int i = 0; i < total; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println();
     }
 }
